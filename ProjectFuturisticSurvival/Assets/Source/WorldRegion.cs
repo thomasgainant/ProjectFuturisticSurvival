764fb344a727bf9d20a48088c36b7dbb4ffce4dd
thomasgainant <thomasgainant@gmail.com>
2018-01-03 14:02:17 +0100

    Cosmic egg waiting to hatch

---
+++ ProjectFuturisticSurvival/Assets/Source/WorldRegion.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldRegion : Entity {
+
+    public static int REGIONSIZE = 1000;//Number of WorldRegionPoints
+    public static float REGIONPRECISION = 1.0f;//World space between two WorldRegionPoints at lod 1
+
+    public WorldRegionPoint[,] points;
+
+	public override void init()
+    {
+        base.init();
+
+        this.generate();
+        this.display(1);
+    }
+
+    protected override void update()
+    {
+        base.update();
+    }
+
+    private void generate()
+    {
+        this.points = new WorldRegionPoint[REGIONSIZE,REGIONSIZE];
+
+    }
+
+    private void display(int lod)
+    {
+
+    }
+}
---
+++ ProjectFuturisticSurvival/Assets/Source/WorldRegionPoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldRegionPoint : Entity {
+
+    public override void init()
+    {
+        base.init();
+    }
+
+    protected override void update()
+    {
+        base.update();
+    }
+}
